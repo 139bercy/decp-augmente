cc1f8754f51947651e2455f823158ac4ae19928a
QMaire <maire.quentin@orange.fr>
2021-04-16 09:47:55 +0200

    Premier ajout Log

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import nettoyage
 import enrichissement
+import logging
 
+#logging.config.fileConfig('logging.conf')
+# create logger
+#logger = logging.getLogger('main.py')
+
+#logging.config.fileConfig('logging.conf')
+# create logger
+#logger2 = logging.getLogger('nettoyage.py')
 
 def main():
+    #logger.info("Début du script de nettoyage")
     nettoyage.main()
+    #logger.info("Fin du script de nettoyage")
+    #logger.info("Début du script d'enrichissement des données")
     enrichissement.main()
+    #logger.info("Fin du script d'enrichissement")
 
 
 if __name__ == "__main__":
