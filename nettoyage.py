a1560d4a794ea2ea697dac85e83c56a69217f89a
Gaspard <gaspard.houillon@depinfonancy.net>
2022-12-02 10:43:41 +0100

    Ajout création du chemin data/

--- nettoyage.py
+++ nettoyage.py
@@ -33,7 +33,9 @@ with open(os.path.join("confs", "var_debug.json")) as f:
 
 path_to_data = conf_data["path_to_data"]
 decp_file_name = conf_data["decp_file_name"]
-
+path_to_data = conf_data["path_to_data"]
+if not(os.path.exists(path_to_data)): # Si le chemin data n'existe pas (dans le cas de la CI et de Saagie)
+    os.mkdir(path_to_data)
 
 def main():
 
