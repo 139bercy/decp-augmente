b13d67c4bc5b197e2379aceb677571ad186c7b76
Gaspard <gaspard.houillon@depinfonancy.net>
2022-12-16 16:57:16 +0100

    correctif typo

---
+++ get_daily_data_v2.py
@@ -0,0 +1,20 @@
+import wget
+import os
+import json
+
+#url_decp = os.environ.get("URL_DECP")
+
+directory_data="data"
+api_key = os.environ.get("API_KEY_GASPARD")
+url_v2 = "https://data.economie.gouv.fr/api/v2/catalog/datasets/decp-rama-v2/exports/json?limit=-1&offset=0&timezone=UTC&apikey="+str(api_key)
+os.makedirs(directory_data, exist_ok=True)
+wget.download(url_v2+str(api_key), os.path.join(directory_data, "decp.json"))
+
+# Comme ça vient de data-eco le format du JSON est un peu différent, on le ré écrit pour le mettre dans le même format que le v1
+with open(os.path.join(directory_data, "decp.json"), "r") as file:
+    fv2_json = json.load(file)
+
+fv1_json = {"marches":fv2_json}
+
+with open(os.path.join(directory_data, "decp.json"), "w") as file:
+    json.dump(fv1_json, file)
