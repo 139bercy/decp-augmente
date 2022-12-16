f6f4244be8d073e6d154803af370cbc6adaa477e
Gaspard <gaspard.houillon@depinfonancy.net>
2022-12-16 11:38:07 +0100

    Debug du crash lié aux objets Nan des concessions

--- nettoyage.py
+++ nettoyage.py
@@ -628,18 +628,12 @@ def indice_marche_avec_modification(data: dict) -> list:
     Retour:
         - list
     """
-    s = 0
     liste_indices = []
     for i in range(len(data['marches'])):
         # Ajout d'un identifiant technique -> Permet d'avoir une colonne id unique par marché
         data["marches"][i]["id_technique"] = i
-        try : 
-            s+=1
-            if data["marches"][i]["modifications"]:
-                liste_indices += [i]
-        except:
-            print('EXCEPT ', s)
-            data["marches"][i]
+        if data["marches"][i]["modifications"]:
+            liste_indices += [i]
     return liste_indices
 
 
@@ -798,9 +792,10 @@ def regroupement_marche(df: pd.DataFrame, dict_modification: dict) -> pd.DataFra
     for objet_marche in liste_objet: # C'est du dedoublonnage en fait ça
         # Récupération du dataframe modification et du dataframe source
         # On créée la colonne "idtech"
-        marche, marche_init = split_dataframe(df, subdata_modif, objet_marche)
-        marche_init["idtech"] = marche.iloc[-1].id_technique
-        marches_init.append(marche_init)
+        if type(objet_marche) == str: # Lorsque y'a un Nan (ça arrive) ça fait tout buguer, alors on évite ce cas là.
+            marche, marche_init = split_dataframe(df, subdata_modif, objet_marche)
+            marche_init["idtech"] = marche.iloc[-1].id_technique
+            marches_init.append(marche_init)
     df_to_concatene = pd.concat([x for x in marches_init], copy=False)
     df.update(df_to_concatene)
     df["idMarche"] = np.where(df.idtech != "", df.idtech, df.id_technique)
