a2670ed138eb08db23b034ed7f7a0c9fdee84049
Gaspard <gaspard.houillon@depinfonancy.net>
2023-01-09 16:00:07 +0100

    retrait de sudo dans apt install lftp

--- upload_dataeco.py
+++ upload_dataeco.py
@@ -49,7 +49,7 @@ with open(path_file_to_upload, "rb") as f:
     df = pickle.load(f)
 path_file_to_upload_csv = path_file_to_upload[:-4]+".csv"
 df.to_csv(path_file_to_upload_csv, quoting=csv.QUOTE_NONNUMERIC, sep=";", index=False)
-bash_cmd_install = [f"sudo apt install lftp"] # Faute de mieux, on installe lftp ainsi.
+bash_cmd_install = [f"apt install lftp"] # Faute de mieux, on installe lftp ainsi.
 subprocess.call(bash_cmd_install, shell=True)
 bash_cmd = [f"lftp -u {USER_DATAECO}:{PWD_DATAECO} {HOST_DATAECO} -e 'cd decp; put {path_file_to_upload_csv}; quit'"] # Je n'ai pas trouvé de biblio ftp python satisfaisante. Donc ce sera en bash
 subprocess.call(bash_cmd, shell=True)
