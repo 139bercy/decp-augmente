f7b5c8b67d3bfe29403b658e6fb49c3ae7a05d6e
kaa-serpent <guillaumedecadoudal@gmail.com>
2023-01-24 15:01:06 +0100

    pep8 : typo

--- test_enrichisement.py
+++ test_enrichisement.py
@@ -2,22 +2,25 @@ import pandas as pd
 import enrichissement
 
 """Peut prendre du temps si on ne commente pas la ligne utils.download_data_enrichissement() dans enrichissement.
-Pour les tests il vaut mieux avoir les fichiers en local puis comenter la ligne et executer pytest.
+Pour les tests il vaut mieux avoir les fichiers en local puis commenter la ligne et executer pytest.
 
 Ces tests sont principalement des tests de non-régression. Qu'on s'assure que les modifications faites ne perturbent pas certains fonctionnement.
 """
+
 
 def test_enrichissement_acheteur():
     df = pd.DataFrame(columns=["siret", "acheteur.id"], data=[['21740276700016', '21740276700016']])
     df = enrichissement.enrichissement_acheteur(df)
     assert df.loc[0, "libelleCommuneAcheteur"] == "TANINGES"
 
+
 def test_renommage_et_recategorisation():
-    df = pd.DataFrame(columns=["nomAcheteur", "idAcheteur", "sirenEtablissement", "denominationSociale_x"], data=[['MauvaisNom', '24840025100158', "877785295", "UneDenomination"],['MauvaisNom', '24840025100158', "877785295", "UneDenomination"] ])
+    df = pd.DataFrame(columns=["nomAcheteur", "idAcheteur", "sirenEtablissement", "denominationSociale_x"], data=[['MauvaisNom', '24840025100158', "877785295", "UneDenomination"],['MauvaisNom', '24840025100158', "877785295", "UneDenomination"]])
     df = enrichissement.renommage_et_recategorisation(df)
     assert (df.nomAcheteur.values == ["COMMUNAUTE D'AGGLOMERATION DU GRAND AVIGNON"]*2).all()
     assert (df.denominationUniteLegale.values == ["MK ETANCHEITE"]*2).all()
     assert (df.loc[:, "denominationSociale_x"].values == ["UneDenomination"]*2).all()
+
 
 def test_apply_luhn():
     df = pd.DataFrame(columns=["idAcheteur", "sirenEtablissement", "siretEtablissement", "typeIdentifiantEtablissement"], data=[['24840025100158', "877785295", "87778529500000", "SIRET"]])   
@@ -26,6 +29,7 @@ def test_apply_luhn():
     assert df.sirenEtablissementValide.values == True
     assert df.siretEtablissementValide.values == "False" # En effet il est faux, les deux premiers sont extrait de vrais entités (cf les deux fonctions plus haut). le siret est créé sans considération dela formule.
     # Pourquoi a-t-on un string au lieu d'un boolean ? A cause du np.where où l'une des possibilités est un string. Ce qui donne une colonne de string. Donc un "False"
+
 
 def test_enrichissement_cpv():
     """
